e6187131b63d65c1c5d2ed984d3d97fe84db5c42
santi-misael <santiagoalamos2000@gmail.com>
2022-12-21 18:13:53 -0300

    pack front

--- client/src/config/environment.js
+++ client/src/config/environment.js
@@ -1,5 +1,3 @@
-const { REACT_APP_API_URL, REACT_APP_API_PORT} = process.env;
-export const API_URL = 
-    REACT_APP_API_URL === "localhost" ?
-        `http://${REACT_APP_API_URL}:${REACT_APP_API_PORT}` : 
-        `https://${REACT_APP_API_URL}`;
+const { REACT_APP_API_PORT, REACT_APP_API_URL} = process.env
+
+export const API_URL = REACT_APP_API_URL === "localhost" ? `http://${REACT_APP_API_URL}:${REACT_APP_API_PORT}` : `https://${REACT_APP_API_URL}`;
