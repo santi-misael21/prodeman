cf38cbda12184cdd3cf98e7aa821fc5772b2d89b
santi-misael <santiagoalamos2000@gmail.com>
2022-12-21 15:48:31 -0300

    port

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { PORT } = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => { //true olvida, false recuerda
-  server.listen(PORT, () => {
+  server.listen('3001', () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
