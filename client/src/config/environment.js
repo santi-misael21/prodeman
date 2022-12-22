c0c1e96a9e6ece50b0e47c53d71cdb610f707ae6
santi-misael <santiagoalamos2000@gmail.com>
2022-12-22 04:12:11 -0300

    environment ok

--- client/src/config/environment.js
+++ client/src/config/environment.js
@@ -2,8 +2,8 @@ const { REACT_APP_API_URL} = process.env
 
 // export const API_URL = REACT_APP_API_URL === "localhost" ? `http://${REACT_APP_API_URL}:${REACT_APP_API_PORT}` : `https://${REACT_APP_API_URL}`;
 
-// export const API_URL = !REACT_APP_API_URL ? `http://localhost:3001` : `https://${REACT_APP_API_URL}`;
-export const API_URL = "https://prodeman.onrender.com";
+export const API_URL = !REACT_APP_API_URL ? `http://localhost:3001` : `https://${REACT_APP_API_URL}`;
+// export const API_URL = "https://prodeman.onrender.com";
 
 /*REACT_APP_API_URL=localhost
 REACT_APP_API_PORT=3001 */
